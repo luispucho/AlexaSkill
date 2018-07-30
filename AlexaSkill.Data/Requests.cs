f5658fc24125c4489108cd415004004f38eb10c6
Luis Alejandro Gomez <luis.gr@outlook.com>
2018-07-30 14:52:44 -0500

    Add project files.

---
+++ AlexaSkill.Data/Requests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlexaSkill.Data
+{
+    public class Requests
+    {
+        public Request Create(Request request)
+        {
+            //var x = ConfigurationManager.ConnectionStrings["EpicorAlexa_dbEntities"].ToString();
+
+            using (var db = new EpicorAlexa_dbEntity())
+            {
+                
+                var member = db.Members.FirstOrDefault(m => m.AlexaUserId == request.UserId);
+
+                if (member == null)
+                {
+                    request.Member = new Member() { AlexaUserId = request.UserId, CreatedDate = DateTime.UtcNow, LastRequestDate = DateTime.UtcNow, RequestCount = 1 };
+
+                    db.Requests.Add(request);
+                }
+                else
+                {
+                    member.Requests.Add(request);
+                }
+
+                db.SaveChanges();
+            }
+
+            return request;
+        }
+    }
+}
---
+++ AlexaSkill/Controllers/AlexaController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.ModelBinding;
+using AlexaSkill;
+
+namespace AlexaSkill.Controllers
+{
+    public class AlexaController : ApiController
+    {
+        [HttpPost, Route("api/alexa/demo")]
+        public dynamic Epicor(AlexaSkill.Data.AlexaRequest alexaRequest)
+        {
+            new Data.Requests().Create(new Data.Request
+            {
+                MemberId = alexaRequest.Session.Attributes?.MemberId ?? 0,
+                Timestamp = alexaRequest.Request.Timestamp,
+                Intent = (alexaRequest.Request.Intent == null) ? "" : alexaRequest.Request.Intent.Name,
+                AppId = alexaRequest.Session.Application.ApplicationId,
+                RequestId = alexaRequest.Request.RequestId,
+                SessionId = alexaRequest.Session.SessionId,
+                UserId = alexaRequest.Session.User.UserId,
+                IsNew = alexaRequest.Session.New,
+                Version = alexaRequest.Version,
+                Type = alexaRequest.Request.Type,
+                Reason = alexaRequest.Request.Reason,
+                //SlotsList = alexaRequest.Request.Intent.GetSlots(),
+                DateCreated = DateTime.UtcNow
+            });
+
+            return new
+            {
+                version = "1.0",
+                SessionAttributes = new { },
+                response = new 
+                {
+                    outputSpeech = new
+                    {
+                        type = "PlainText",
+                        text = "Hello happy World!",
+                    },
+                    card = new
+                    {
+                        type = "Simple",
+                        title = "Epicor",
+                        content = "Hello funny world!"
+                    },
+                    shouldEndSession = true
+                }
+            };
+        }
+    }
+}
